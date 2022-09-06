b32a5be2e96e6c326c9723bd134dd4393d072ea0
jg602 <jg602@JUANJOS>
2022-09-06 14:39:29 -0700

    Agregar archivos de proyecto.

---
+++ get y set/Clasemul.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_y_set
+{
+    internal class Clasemul
+    {
+        private int n1;
+        private int n2;
+
+        public Clasemul()
+        {
+
+        }
+        
+        public int N1
+        {
+            get { return n1; }
+            set { n1 = value; }
+        }
+        public int N2
+        {
+            get { return n2; }
+            set { n2 = value; }
+        }
+
+        public int mul() 
+        { 
+            return n1 * n2;
+        }
+    }
+}
---
+++ get y set/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace get_y_set
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            Clasemul OO = new Clasemul();
+            OO.N1 = (int.Parse(textBox1.Text));
+            OO.N2 = (int.Parse(textBox2.Text));
+            textBox3.Text = OO.mul().ToString();
+        }
+    }
+}
